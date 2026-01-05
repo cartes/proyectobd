b8786d6c2a09f945a8f1bb242b591b01963a7cd0
Cristian Cartes <cristiancartesa@gmail.com>
2026-01-05 17:20:46 -0300

    🚀 Integración completa Mercado Pago

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\WebhookController;
+
+// Autenticadas
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Subscriptions
+    Route::prefix('subscriptions')->group(function () {
+        Route::get('/', [SubscriptionController::class, 'apiIndex']);
+        Route::get('/{id}', [SubscriptionController::class, 'apiShow']);
+        Route::delete('/{id}', [SubscriptionController::class, 'apiDestroy']);
+    });
+
+    // Purchases
+    Route::prefix('purchases')->group(function () {
+        Route::post('/', [PurchaseController::class, 'apiStore']);
+        Route::get('/', [PurchaseController::class, 'apiIndex']);
+        Route::get('/{id}', [PurchaseController::class, 'apiShow']);
+    });
+});
+
+// PÚBLICAS (webhook DEBE ser público)
+Route::post('/webhook/mercadopago', [WebhookController::class, 'handleMercadoPagoWebhook']);
