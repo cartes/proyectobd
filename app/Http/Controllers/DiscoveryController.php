8c5a342997f5ed0d25eba823561355d218a24fe5
Cristian Cartes <cristiancartesa@gmail.com>
2026-01-29 11:35:41 -0300

    Cambios para pasar los tests

--- app/Http/Controllers/DiscoveryController.php
+++ app/Http/Controllers/DiscoveryController.php
@@ -28,7 +28,7 @@ class DiscoveryController extends Controller
 
         // Filtro por ciudad (updated)
         if ($request->filled('city')) {
-            $query->where('city', 'LIKE', "%{$request->city}%");
+            $query->where('city', 'LIKE', '%' . $request->city . '%');
         }
 
         // Filtro por intereses
