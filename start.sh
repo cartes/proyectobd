aaf1b445bf960fadb49e16b0aab08653a90cd61a
Cristian Cartes <cristiancartesa@gmail.com>
2026-02-04 17:31:32 -0300

    arreglando crush en deploy 03

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "Starting server..."
 # Explicitly use php.ini via -c flag and start built-in server via server.php
 # This avoids overriding system extension loading via environment variables
-php -c php.ini -S 0.0.0.0:$PORT server.php
+php -c php.ini -S 0.0.0.0:$PORT -t public server.php
